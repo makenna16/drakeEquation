6b70ec2fb1092d904d721d73c4c307b4afd609db
makenna <makenna.gordon.2023@student.lhschools.org>
2020-02-28 12:55:35 -0500

    Added f_p

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,5 +1,7 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,9 +11,9 @@ public class Main {
 
     public static void drakeEquation(){
 /*Drake equation
-N = number of intelligent species that can communicate with us
-R∗ = 1 yr−1 (1 star formed per year, on the average over the life of the galaxy; this was regarded as conservative)
-fp = 0.2 to 0.5 (one fifth to one half of all stars formed will have planets)
+ N = number of intelligent species that can communicate with us
+ R∗ = 1 yr−1 (1 star formed per year, on the average over the life of the galaxy; this was regarded as conservative)
+ fp = 0.2 to 0.5 (one fifth to one half of all stars formed will have planets)
 ne = 1 to 5 (stars with planets will have between 1 and 5 planets capable of developing life)
 fl = 1 (100% of these planets will develop life)
 fi = 1 (100% of which will develop intelligent life)
@@ -27,14 +29,32 @@ L = 1000 to 100,000,000 years (which will last somewhere between 1000 and 100,00
     double f_c;
     int L;
 
+    //todo initialize the scanner to take input and commit
+    //Ask for rStar number store in the rStar variable and commit
+    //Ask for f_p fraction store in the f_p variable and commit
+    //Ask for numberOfExoplanets number store in the numberOfExoplanets variable and commit
+    //Ask for f_l fraction store in the f_l variable and commit
+    //Ask for f_i fraction store in the f_i variable and commit
+    //Ask for f_c fraction store in the f_c variable and commit
+    //Ask for L number store in the L variable and commit
     Scanner keyboard;
     keyboard = new Scanner(System.in);
+
     System.out.println("How many stars form on average per year(1-10)?");
     rStar = keyboard.nextInt();
+
+    System.out.println("How many star that were formed in a year will have planets (0.2-0.5)?");
+    f_p = keyboard.nextDouble();
+
+    System.out.println("");
+
+
     N = rStar * f_p *numberOfExoplanets *f_l *f_i *f_c * L;
+
 
     System.out.println("The number of intelligent species that are out there \n" +
             "that can communicate with us is: " + N);
+    System.out.println();
 
 
 
